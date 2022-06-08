a5adad4f51cdfd8f5c21405fccd75cea9b83f7d1
Mathenge-Alex <arxmaina@gmail.com>
2022-06-08 10:35:44 +0300

    Feat: Add follow and Image classes

--- insta/models.py
+++ insta/models.py
@@ -32,3 +32,42 @@ class Profile(models.Model):
         self.user()
 
 
+class Follow(models.Model): 
+    posted = models.DateTimeField(auto_now_add=True)
+    followed = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='profile_followed')
+    follower = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='profile_following')
+
+    def __str__(self):
+        return self.pk 
+
+class Image(models.Model):
+    image = CloudinaryField('image')
+    name = models.CharField(max_length = 30)
+    caption = models.TextField(blank= True)
+    post_date = models.DateTimeField(auto_now_add=True)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    likes = models.ManyToManyField(Profile, related_name="posts")
+    
+    def __str__(self):
+        return self.name
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    def update_caption(self, new_caption):
+        self.caption = new_caption
+        self.save()
+
+    def like_count(self):
+        return self.likes.count()
+
+    @classmethod
+    def get_profile_images(cls,profile):
+        return cls.objects.filter(profile = profile)
+
+    class Meta:
+        ordering = ['-post_date']
+
